4210ce36c6f14208e1de46959078c877eef496ee
Ryan Phillips <rphillips@redhat.com>
2018-10-15 21:33:42 -0500

    start check pointer within kube-system
    TODO: change to different namespace

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -6,12 +6,14 @@ import (
 	"github.com/openshift/pod-checkpointer-operator/pkg/apis/pod/v1alpha1"
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"github.com/prometheus/client_golang/prometheus"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func NewHandler(m *Metrics) sdk.Handler {
@@ -34,7 +36,7 @@ type Handler struct {
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	switch o := event.Object.(type) {
 	case *v1alpha1.PodCheckpointerOperator:
-		err := sdk.Create(newbusyBoxPod(o))
+		err := sdk.Create(newCheckpointerDaemonSet(o))
 		if err != nil && !errors.IsAlreadyExists(err) {
 			logrus.Errorf("failed to create busybox pod : %v", err)
 			// increment error metric
@@ -45,19 +47,16 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	return nil
 }
 
-// newbusyBoxPod demonstrates how to create a busybox pod
-func newbusyBoxPod(cr *v1alpha1.PodCheckpointerOperator) *corev1.Pod {
-	labels := map[string]string{
-		"app": "busy-box",
-	}
-	return &corev1.Pod{
+// newCheckpointerDaemonSet demonstrates how to create a busybox pod
+func newCheckpointerDaemonSet(cr *v1alpha1.PodCheckpointerOperator) *appsv1.DaemonSet {
+	return &appsv1.DaemonSet{
 		TypeMeta: metav1.TypeMeta{
-			Kind:       "Pod",
-			APIVersion: "v1",
+			Kind:       "DaemonSet",
+			APIVersion: "apps/v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "busy-box",
-			Namespace: cr.Namespace,
+			Name:      "pod-checkpointer",
+			Namespace: "kube-system",
 			OwnerReferences: []metav1.OwnerReference{
 				*metav1.NewControllerRef(cr, schema.GroupVersionKind{
 					Group:   v1alpha1.SchemeGroupVersion.Group,
@@ -65,14 +64,127 @@ func newbusyBoxPod(cr *v1alpha1.PodCheckpointerOperator) *corev1.Pod {
 					Kind:    "PodCheckpointerOperator",
 				}),
 			},
-			Labels: labels,
+			Labels: map[string]string{
+				"tier":    "control-plane",
+				"k8s-app": "pod-checkpointer",
+			},
 		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "docker.io/busybox",
-					Command: []string{"sleep", "3600"},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"tier":    "control-plane",
+					"k8s-app": "pod-checkpointer",
+				},
+			},
+			UpdateStrategy: appsv1.DaemonSetUpdateStrategy{
+				Type: appsv1.RollingUpdateDaemonSetStrategyType,
+				RollingUpdate: &appsv1.RollingUpdateDaemonSet{
+					MaxUnavailable: func() *intstr.IntOrString { i := intstr.FromInt(1); return &i }(),
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"tier":    "control-plane",
+						"k8s-app": "pod-checkpointer",
+					},
+					Annotations: map[string]string{
+						"checkpointer.alpha.coreos.com/checkpoint": "true",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Volumes: []corev1.Volume{
+						corev1.Volume{
+							Name: "kubeconfig",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "kubeconfig-in-cluster",
+									},
+								},
+							},
+						},
+						corev1.Volume{
+							Name: "etc-kubernetes",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/etc/kubernetes",
+								},
+							},
+						},
+						corev1.Volume{
+							Name: "var-run",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/var/run",
+								},
+							},
+						},
+					},
+					Containers: []corev1.Container{
+						{
+							Name:  "pod-checkpointer",
+							Image: "quay.io/coreos/pod-checkpointer:9dc83e1ab3bc36ca25c9f7c18ddef1b91d4a0558",
+							VolumeMounts: []corev1.VolumeMount{
+								corev1.VolumeMount{
+									Name:      "kubeconfig",
+									MountPath: "/etc/checkpointer",
+								},
+								corev1.VolumeMount{
+									Name:      "etc-kubernetes",
+									MountPath: "/etc/kubernetes",
+								},
+								corev1.VolumeMount{
+									Name:      "var-run",
+									MountPath: "/var/run",
+								},
+							},
+							Env: []corev1.EnvVar{
+								corev1.EnvVar{
+									Name: "NODE_NAME",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "spec.nodeName",
+										},
+									},
+								},
+								corev1.EnvVar{
+									Name: "POD_NAME",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "metadata.name",
+										},
+									},
+								},
+								corev1.EnvVar{
+									Name: "POD_NAMESPACE",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "metadata.namespace",
+										},
+									},
+								},
+							},
+							Command: []string{
+								"/checkpoint",
+								"--lock-file=/var/run/lock/pod-checkpointer.lock",
+								"--kubeconfig=/etc/checkpointer/kubeconfig",
+								"--checkpoint-grace-period=5m",
+							},
+						},
+					},
+					NodeSelector: map[string]string{
+						"node-role.kubernetes.io/master": "",
+					},
+					RestartPolicy:      corev1.RestartPolicyAlways,
+					ServiceAccountName: "pod-checkpointer",
+					Tolerations: []corev1.Toleration{
+						corev1.Toleration{
+							Key:      "node-role.kubernetes.io/master",
+							Operator: corev1.TolerationOpExists,
+							Effect:   corev1.TaintEffectNoSchedule,
+						},
+					},
 				},
 			},
 		},
